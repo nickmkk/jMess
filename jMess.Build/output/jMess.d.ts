a596ecbb374e2818f0024dfc66b628af8b5958cd
Nicholas J. Markkula <nickmkk@gmail.com>
2017-05-29 06:21:11 -0500

    added generics to the event registry typescript definitions

--- jMess.Build/output/jMess.d.ts
+++ jMess.Build/output/jMess.d.ts
@@ -7,10 +7,10 @@ declare module jMess {
         private _onRaise;
         constructor(logR: ILogR, onRaise: (event: string, data: Object) => void, timeout?: (delegate: () => void, delay: number) => void);
         getAvailableEvents(): string[];
-        getHooksForEvent(eventName: string): Function[];
-        hook(eventToHook: string, delegate: Function): () => void;
-        hookOnce(eventToHook: string, delegate: Function): void;
-        raise(eventToRaise: string, data: Object): void;
+        getHooksForEvent<T>(eventName: string): { (data: T): void; }[];
+        hook<T>(eventToHook: string, delegate: (data: T) => void): () => void;
+        hookOnce<T>(eventToHook: string, delegate: (data: T) => void): void;
+        raise<T>(eventToRaise: string, data: T): void;
         register(eventsToRegister: string | string[] | Object): void;
         private _registerEventsObject(eventsObj);
         private _registerArrayOfEvents(eventsArray);
@@ -21,9 +21,9 @@ declare module jMess {
 declare module jMess {
     interface IEventRegistry {
         getAvailableEvents(): string[];
-        hook(eventName: string, onRaise: Function): () => void;
-        hookOnce(eventToHook: string, delegate: Function): any;
-        raise(eventToRaise: string, data: Object): void;
         register(eventsToRegister: any): void;
+        hook<T>(eventName: string, onRaise: (data: T) => void): () => void;
+        hookOnce<T>(eventToHook: string, delegate: (data: T) => void): any;
+        raise<T>(eventToRaise: string, data: T): void;
     }
 }
